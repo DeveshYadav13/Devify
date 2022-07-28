b4d7f7265c228f73c9a0ca85a8aace4ecfa275ab
Devesh <deveshamway@gmail.com>
2022-07-28 18:41:18 +0530

    Fixing not loading images error

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -202,6 +202,3 @@ TEMPLATE_DIRS = (
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-if os.getcwd() == '/app':
-    DEBUG = False
